dce840501e54f605420bbf4fdaf5f157bf215724
MSNeotech <m.sorrentino@neotech.srl>
2022-10-26 19:22:15 +0200

    Seconda commit

--- TestGit/src/srl/neotech/corsojava/testgit/Startup.java
+++ TestGit/src/srl/neotech/corsojava/testgit/Startup.java
@@ -1,18 +1,12 @@
-package srl.neotech.corsojava.robotagliaerba;
+package srl.neotech.corsojava.testgit;
 
 public class Startup {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World");
 
-	
-		
-		
-		
-		 
-		
-			
 	}
+
 }
-
-
