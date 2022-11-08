8ae6bd4b4efe35135f705c87c4ed2997a7fd2c67
Ms <m.sorrentino@neotech.srl>
2022-11-08 10:03:09 +0100

    Esercizio calcolo esame completato

---
+++ CalcoloEsame/src/srl/neotech/corsojava/calcoloesame/Startup.java
@@ -0,0 +1,34 @@
+package srl.neotech.corsojava.calcoloesame;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Startup {
+
+	static ArrayList<Integer> voti = new ArrayList<Integer>();
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Random r = new Random();
+		
+		int votoScritto = r.nextInt(-8,8);
+		voti.add(0, votoScritto);
+		int votoOrale= r.nextInt(0,24);
+		voti.add(1,votoOrale);
+		
+		Integer ris = voti.get(0) + voti.get(1);
+		
+		System.out.println("Voto scritto: " +voti.get(0)+"\n");
+		System.out.println("Voto orale: " +voti.get(1)+"\n");
+		System.out.println("Risultato:" +ris +"\n");
+		
+		if(voti.get(0)<=0 && ris>18) System.out.println("Bocciato");
+		else if(voti.get(0)<=0 && voti.get(1)<18) System.out.println("Bocciato");
+		else if(voti.get(0)>0 && ris<18) System.out.println("Bocciato");
+		else if(ris==31 || ris==32) System.out.println("Congratulazione: 30 e lode");
+		else System.out.println("Promosso");
+
+	}
+
+}
